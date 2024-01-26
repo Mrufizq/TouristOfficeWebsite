246abab88738c613e7085b00e5e6a2997c64df64
Mrufizq <157791766+Mrufizq@users.noreply.github.com>
2024-01-26 09:57:31 +0100

    Add files via upload

---
+++ Styles/Script/routes.js
@@ -0,0 +1,116 @@
+function showContent1() {
+    var elementos = document.getElementsByClassName("day");
+    elementos[1].style.backgroundColor = '#2B335A'
+    elementos[1].style.color = 'white'
+    elementos[2].style.backgroundColor = '#2B335A'
+    elementos[2].style.color = 'white'
+    elementos[3].style.backgroundColor = '#2B335A'
+    elementos[3].style.color = 'white'
+    elementos[4].style.backgroundColor = '#2B335A'
+    elementos[4].style.color = 'white'
+    document.getElementById('content2').style.display = 'none' 
+    document.getElementById('content3').style.display = 'none' 
+    document.getElementById('content4').style.display = 'none' 
+    document.getElementById('content5').style.display = 'none'  
+    document.getElementById('content1').style.display = 'flex'
+
+  
+    document.getElementById('day1').style.color = 'black'
+    document.getElementById('day1').style.backgroundColor = 'white'
+}
+function showContent2() {
+    var elementos = document.getElementsByClassName("day");
+   
+    
+    elementos[0].style.backgroundColor = '#2B335A'
+    elementos[0].style.color = 'white'
+    elementos[2].style.backgroundColor = '#2B335A'
+    elementos[2].style.color = 'white'
+    elementos[3].style.backgroundColor = '#2B335A'
+    elementos[3].style.color = 'white'
+    elementos[4].style.backgroundColor = '#2B335A'
+    elementos[4].style.color = 'white'
+   
+    document.getElementById('content1').style.display = 'none' 
+    document.getElementById('content3').style.display = 'none' 
+    document.getElementById('content4').style.display = 'none' 
+    document.getElementById('content5').style.display = 'none'  
+    document.getElementById('content2').style.display = 'flex'
+ 
+ 
+    document.getElementById('day2').style.color = 'black'
+    document.getElementById('day2').style.backgroundColor = 'white'
+ 
+   
+}
+function showContent3() {
+    var elementos = document.getElementsByClassName("day");
+   
+    
+    elementos[0].style.backgroundColor = '#2B335A'
+    elementos[0].style.color = 'white'
+    elementos[1].style.backgroundColor = '#2B335A'
+    elementos[1].style.color = 'white'
+    elementos[3].style.backgroundColor = '#2B335A'
+    elementos[3].style.color = 'white'
+    elementos[4].style.backgroundColor = '#2B335A'
+    elementos[4].style.color = 'white'
+   
+    document.getElementById('content1').style.display = 'none' 
+    document.getElementById('content2').style.display = 'none' 
+    document.getElementById('content4').style.display = 'none' 
+    document.getElementById('content5').style.display = 'none'  
+    document.getElementById('content3').style.display ='flex'
+ 
+    document.getElementById('day3').style.color = 'black'
+    document.getElementById('day3').style.backgroundColor = 'white'
+ 
+   
+}
+function showContent4() {
+    var elementos = document.getElementsByClassName("day");
+   
+    
+    elementos[0].style.backgroundColor = '#2B335A'
+    elementos[0].style.color = 'white'
+    elementos[2].style.backgroundColor = '#2B335A'
+    elementos[2].style.color = 'white'
+    elementos[1].style.backgroundColor = '#2B335A'
+    elementos[1].style.color = 'white'
+    elementos[4].style.backgroundColor = '#2B335A'
+    elementos[4].style.color = 'white'
+    document.getElementById('content2').style.display = 'none' 
+    document.getElementById('content1').style.display = 'none' 
+    document.getElementById('content3').style.display = 'none' 
+    document.getElementById('content5').style.display = 'none'  
+    document.getElementById('content4').style.display = 'flex'
+ 
+    document.getElementById('day4').style.color = 'black'
+    document.getElementById('day4').style.backgroundColor = 'white'
+ 
+   
+}
+function showContent5() {
+    var elementos = document.getElementsByClassName("day");
+   
+    
+    elementos[0].style.backgroundColor = '#2B335A'
+    elementos[0].style.color = 'white'
+    elementos[2].style.backgroundColor = '#2B335A'
+    elementos[2].style.color = 'white'
+    elementos[3].style.backgroundColor = '#2B335A'
+    elementos[3].style.color = 'white'
+    elementos[1].style.backgroundColor = '#2B335A'
+    elementos[1].style.color = 'white'
+   
+    document.getElementById('content2').style.display = 'none' 
+    document.getElementById('content3').style.display = 'none' 
+    document.getElementById('content4').style.display = 'none' 
+    document.getElementById('content1').style.display = 'none'  
+    document.getElementById('content5').style.display = 'flex'
+ 
+    document.getElementById('day5').style.color = 'black'
+    document.getElementById('day5').style.backgroundColor = 'white'
+ 
+   
+}
